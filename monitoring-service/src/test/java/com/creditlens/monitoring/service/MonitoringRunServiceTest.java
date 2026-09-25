2c2ecd5bebaa2b4e6607a4d57a643f22373a30da
Anastasiya_Radkevich <v-aradkevich@expediagroup.com>
2026-09-25 21:59:39 +0300

    Run monitoring email delivery every minute

--- monitoring-service/src/test/java/com/creditlens/monitoring/service/MonitoringRunServiceTest.java
+++ monitoring-service/src/test/java/com/creditlens/monitoring/service/MonitoringRunServiceTest.java
@@ -139,7 +139,7 @@ class MonitoringRunServiceTest {
         new MonitoringReportRenderer(),
         mail,
         new MonitoringProperties(
-            "0 */5 * * * *", Duration.ofMinutes(5), "pcr_monitoring@dansketest.dk"),
+            "0 * * * * *", Duration.ofMinutes(5), "pcr_monitoring@dansketest.dk"),
         Clock.fixed(NOW, ZoneOffset.UTC));
   }
 
